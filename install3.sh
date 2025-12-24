504a25542f64293322f29b5f67bb90a528b4cb1f
derek_inc <dontnamegg@gmail.com>
2025-12-24 15:29:07 +0000

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -12,8 +12,13 @@ font="noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
 system="bspwm sxhkd librewolf rofi rofi-bluetooth rofi-network-manager alacritty xfe xautolock brightnessctl playerctl asusctl"
 
-echo "install-1"
+echo "install"
 yay -S --needed $driver $font
+
+echo "update"
 yay -Syu --noconfirm
-echo "install-2"
+
+echo "install"
 yay -S --needed $xorg $system
+
+sudo 
