a29c87e7bbe2eeb23dd48cf8d13b52108fae1028
derek_inc <dontnamegg@gmail.com>
2025-12-24 14:04:27 +0000

    Create install3.sh

---
+++ install3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "yay"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd
+rm -rf yay
+
+driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
+font="noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji"
+xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
+system="bspwm sxhkd librewolf rofi rofi-bluetooth rofi-network-manager alacritty xfe xautolock brightnessctl playerctl asusctl"
+
+echo "install"
+yay -S --needed $driver $font $xorg $system
