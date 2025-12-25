7278e260878b18457f1d0f5e2d2ecd7b2ce87140
derek_inc <dontnamegg@gmail.com>
2025-12-25 16:57:27 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -11,10 +11,10 @@ sudo pacman -Syu --noconfirm
 #cd
 
 driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
-font="noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji"
+font="noto-fonts noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
 extra="xautolock brightnessctl playerctl"
-system="bspwm sxhkd librewolf rofi rofi-bluetooth rofi-network-manager alacritty xfe steam  brightnessctl playerctl asusctl"
+#system="bspwm sxhkd librewolf alacritty xfe steam asusctl"
 
 #install
 sudo pacman -S --needed $driver $font $xorg $extra
@@ -25,26 +25,17 @@ git clone 'https://aur.archlinux.org/bspwm-git.git'
 git clone 'https://aur.archlinux.org/sxhkd-git.git'
 git clone 'https://aur.archlinux.org/polybar-git.git'
 git clone 'https://aur.archlinux.org/librewolf-bin.git'
-git clone 'https://aur.archlinux.org/rofi-git.git'
-git clone 'https://aur.archlinux.org/rofi-bluetooth-git.git'
-git clone 'https://aur.archlinux.org/rofi-file-browser-extended-git.git'
 git clone 'https://aur.archlinux.org/alacritty-git.git'
-git clone 'https://aur.archlinux.org/asusctl.git'
+#git clone 'https://aur.archlinux.org/asusctl.git'
 
 
-cd 'bspwm-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'sxhkd-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'polybar-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'librewolf-bin' && makepkg -si --noconfirm && cd '/tmp'
-cd 'rofi-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'rofi-bluetooth-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'rofi-file-browser-extended-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'alacritty-git' && makepkg -si --noconfirm && cd '/tmp'
-cd 'asusctl' && makepkg -si --noconfirm && cd '/tmp'
+cd 'bspwm-git' && makepkg -si && cd '/tmp'
+cd 'sxhkd-git' && makepkg -si && cd '/tmp'
+cd 'polybar-git' && makepkg -si && cd '/tmp'
+cd 'librewolf-bin' && makepkg -si && cd '/tmp'
+cd 'alacritty-git' && makepkg -si && cd '/tmp'
+#cd 'asusctl' && makepkg -si && cd '/tmp'
 
-rm -rf {bspwm-git,sxhkd-git,bspwmbar-git,librewolf-bin,rofi-git,rofi-bluetooth-git,rofi-file-browser-extended-git,alacritty-git,asusctl}
-cd
-
-sudo cp ~/dotfiles/xorg.conf /etc/X11/
-sudo cp ~/dotfiles/.xinitrc ~/
-sudo cp ~/dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+sudo cp ~/linux-setup/dotfiles/xorg.conf /etc/X11/
+sudo cp ~/linux-setup/dotfiles/.xinitrc ~/
+sudo cp ~/linux-setup/dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
