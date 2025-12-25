894bdb74a4cc7131b396e09238883fb53f401dad
derek_inc <dontnamegg@gmail.com>
2025-12-25 18:09:11 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -9,17 +9,17 @@ sudo pacman-key --populate archlinux
 cd /tmp
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si --noconfirm
+makepkg -si --needed --noconfirm
 cd
 
 driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
-font="noto-fonts noto-fonts-cjk noto-fonts-emoji"
+font="noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
 extra="xautolock brightnessctl playerctl asusctl"
 system="bspwm sxhkd polybar librewolf-bin xfe dmenu alacritty steam"
 
 #install
-yay -S $driver $font $xorg $extra $system
+yay -S --needed $driver $font $xorg $extra $system
 
 sudo cp ~/linux-setup/dotfiles/xorg.conf /etc/X11/
 sudo cp ~/linux-setup/dotfiles/.xinitrc ~/
