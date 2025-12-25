14812559ab0d34fd6d8fdb8b75c71e5944514c09
derek_inc <dontnamegg@gmail.com>
2025-12-25 15:28:44 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -4,25 +4,59 @@
 sudo pacman -Syu --noconfirm
 
 #yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd
-rm -rf yay
+#cd /tmp
+#git clone https://aur.archlinux.org/yay-bin.git
+#cd /yay-bin
+#makepkg -si --noconfirm
+#cd
 
 driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
 font="noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
-system="bspwm sxhkd librewolf rofi rofi-bluetooth rofi-network-manager alacritty xfe steam xautolock brightnessctl playerctl asusctl"
+extra="xautolock brightnessctl playerctl"
+system="bspwm sxhkd librewolf rofi rofi-bluetooth rofi-network-manager alacritty xfe steam  brightnessctl playerctl asusctl"
 
 #install
-yay -S --needed $driver $font
+sudo pacman -S --needed $driver $font $xorg $extra
 
-#update
-yay -Syu --noconfirm
+#aur
+cd /tmp
+git clone 'https://aur.archlinux.org/bspwm.git'
+cd /bspwm
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/sxhkd.git'
+cd /sxhkd
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/bspwmbar.git'
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/librewolf.git'
+cd /librewolf-bin
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/rofi.git'
+cd /rofi
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/rofi-bluetooth.git'
+cd /rofi-bluetooth
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/rofi-file-browser-extended.git'
+cd rofi-browser-extended
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/alacritty.git'
+cd /alacritty
+makepkg -si --noconfirm
+cd /tmp
+git clone 'https://aur.archlinux.org/asusctl.git'
+cd /asusctl
+makepkg -si --noconfirm
+cd
 
-#install
-yay -S --needed $xorg $system
 
 sudo cp ~/dotfiles/xorg.conf /etc/X11/
 sudo cp ~/dotfiles/.xinitrc ~/
