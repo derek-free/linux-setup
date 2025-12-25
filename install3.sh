9be79e09ef611e40a5fd1bcb4d4159367fa4c80b
derek_inc <dontnamegg@gmail.com>
2025-12-25 17:06:39 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #update
-sudo pacman -Syu --noconfirm
+sudo pacman -Syu
 
-#yay
-#cd /tmp
-#git clone https://aur.archlinux.org/yay-bin.git
-#cd /yay-bin
-#makepkg -si --noconfirm
-#cd
+yay
+cd /tmp
+git clone https://aur.archlinux.org/yay-bin.git
+cd /yay-bin
+makepkg -si --noconfirm
+cd
 
 driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
 font="noto-fonts noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
-extra="xautolock brightnessctl playerctl"
-#system="bspwm sxhkd librewolf alacritty xfe steam asusctl"
+extra="xautolock brightnessctl playerctl asusctl"
+system="bspwm polybar sxhkd librewolf-bin alacritty steam"
 
 #install
-sudo pacman -S --needed $driver $font $xorg $extra
+yay -S $driver $font $xorg $extra $system
 
 #aur
 cd /tmp
@@ -27,14 +27,6 @@ git clone 'https://aur.archlinux.org/polybar-git.git'
 git clone 'https://aur.archlinux.org/librewolf-bin.git'
 git clone 'https://aur.archlinux.org/alacritty-git.git'
 #git clone 'https://aur.archlinux.org/asusctl.git'
-
-
-cd 'bspwm-git' && makepkg -si && cd '/tmp'
-cd 'sxhkd-git' && makepkg -si && cd '/tmp'
-cd 'polybar-git' && makepkg -si && cd '/tmp'
-cd 'librewolf-bin' && makepkg -si && cd '/tmp'
-cd 'alacritty-git' && makepkg -si && cd '/tmp'
-#cd 'asusctl' && makepkg -si && cd '/tmp'
 
 sudo cp ~/linux-setup/dotfiles/xorg.conf /etc/X11/
 sudo cp ~/linux-setup/dotfiles/.xinitrc ~/
