4df3b6903589f2ea6fd5a2eee14ef80523d11bd6
derek_inc <dontnamegg@gmail.com>
2025-12-25 11:35:46 +0100

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "linux"
+#linux
 pacstrap -K /mnt base linux-lts linux-firmware sof-firmware base-devel sudo nano grub efibootmgr git networkmanager bluez bluez-utils pipewire pipewire-pulse pipewire-alsa alsa-ucm-conf
 
-echo "genfstab"
+#genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "copy-script"
+#copy-script
 mkdir -p /mnt/root/linux-setup
 cp *.sh /mnt/root/linux-setup
 
-echo "arch-chroot /mnt"
+clear
+echo 'arch-chroot /mnt'
