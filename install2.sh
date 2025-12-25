f687c76babb13f0dd255ddc04095aacc2f99af1f
derek_inc <dontnamegg@gmail.com>
2025-12-25 11:34:51 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-echo "date"
-ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+#date
+read -p 'region' region
+read -p 'city' city
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
 hwclock --systohc
 
-echo "locale"
+#locale
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 echo 'ru_RU.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 locale-gen
 
-echo "hostname"
+#hostname
 echo 'archlinux' > /etc/hostname
 
-echo "host"
+#hosts
 echo '127.0.0.1 localhost' > /etc/hosts
 echo '::1 localhost' >> /etc/hosts
 echo '127.0.0.1 archlinux' >> /etc/hosts
 
-echo "host-genshin"
+#hosts-genshin
 echo '0.0.0.0 sg-public-data-api.hoyoverse.com' >> /etc/hosts
 echo '0.0.0.0 log-upload-os.hoyoverse.com' >> /etc/hosts
 
-echo "password-root"
-echo 'root:mahou' | chpasswd
+#password-root
+passwd
 
-echo "password-user"
-useradd -m -G wheel -s /bin/bash derek_inc
-echo 'derek_inc:mahou' | chpasswd
+#password-user
+read -p 'user' user
+useradd -m -G wheel -s /bin/bash $user
+passwd $user
 
-echo "rule-wheel"
+#rule-wheel
 echo '%wheel ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
-echo "grub"
+#grub
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
 echo 'GRUB_DEFAULT=0' > /etc/default/grub
 echo 'GRUB_TIMEOUT=0' >> /etc/default/grub
@@ -46,15 +49,15 @@ echo 'GRUB_GFXPAYLOAD_LINUX=keep' >> /etc/default/grub
 echo 'GRUB_DISABLE_RECOVERY=true' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "multilib"
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+#multilib
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 
-echo "systemctl"
+#systemctl
 systemctl enable NetworkManager bluetooth
 
-echo "move-script"
-mv /root/linux-setup /home/derek_inc/
-chown -R derek_inc:derek_inc /home/derek_inc/linux-setup
+#move-script
+mv /root/linux-setup /home/$user/
+chown -R $user:$user /home/$user/linux-setup
 
 echo "exit & reboot"
