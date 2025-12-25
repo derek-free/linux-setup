9bc33f699d791baf68bf2ca8a0a6e5927f7413d7
derek_inc <dontnamegg@gmail.com>
2025-12-25 17:32:48 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -3,7 +3,7 @@
 #update
 sudo pacman -Syu
 
-yay
+#yay
 cd /tmp
 git clone https://aur.archlinux.org/yay-bin.git
 cd /yay-bin
@@ -14,19 +14,10 @@ driver="mesa intel-ucode vulkan-intel intel-media-driver libva-intel-driver"
 font="noto-fonts noto-fonts-cjk noto-fonts-emoji"
 xorg="xorg-server xorg-xinit xorg-xrandr xorg-xinput xorg-xsetroot xorg-xprop"
 extra="xautolock brightnessctl playerctl asusctl"
-system="bspwm polybar sxhkd librewolf-bin alacritty steam"
+system="bspwm sxhkd polybar librewolf-bin xfe dmenu alacritty steam"
 
 #install
 yay -S $driver $font $xorg $extra $system
-
-#aur
-cd /tmp
-git clone 'https://aur.archlinux.org/bspwm-git.git'
-git clone 'https://aur.archlinux.org/sxhkd-git.git'
-git clone 'https://aur.archlinux.org/polybar-git.git'
-git clone 'https://aur.archlinux.org/librewolf-bin.git'
-git clone 'https://aur.archlinux.org/alacritty-git.git'
-#git clone 'https://aur.archlinux.org/asusctl.git'
 
 sudo cp ~/linux-setup/dotfiles/xorg.conf /etc/X11/
 sudo cp ~/linux-setup/dotfiles/.xinitrc ~/
