f49b34c412ac274b1a662a07c1eb8b3b0013369e
derek_inc <dontnamegg@gmail.com>
2025-12-25 18:00:57 +0100

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #linux
-pacstrap -K /mnt base linux-lts linux-firmware sof-firmware base-devel sudo nano grub efibootmgr networkmanager bluez bluez-utils pipewire pipewire-pulse pipewire-alsa alsa-ucm-conf
+pacstrap -K /mnt base linux-lts linux-firmware sof-firmware base-devel sudo nano grub efibootmgr git networkmanager bluez bluez-utils pipewire pipewire-pulse pipewire-alsa alsa-ucm-conf
 
 #genfstab
 genfstab -U /mnt >> /mnt/etc/fstab
